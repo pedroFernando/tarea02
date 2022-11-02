d552980236f6bb722b288187434dc8c481e56dba
Pedro <fernandoaponte1103@gmail.com>
2022-11-02 00:43:56 -0500

    First commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-27)8rs*ww155zn7vogvz@nrzg)xwks$exri@af-a3k!y)gdud!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
